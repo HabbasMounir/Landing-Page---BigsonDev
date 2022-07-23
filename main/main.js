0bc5c198b80f9cdf2b7d366e1e7f3d5884542ea0
Mounir habbas <Mounirawt@gmail.com>
2022-07-23 17:07:55 +0100

    Wrapped Green V:00.00.04

--- main/main.js
+++ main/main.js
@@ -1,1 +1,19 @@
+let humburgerIcon=document.querySelector("#humburgerIcon")
+let header=document.querySelector("#header")
+let nav__bar= document.querySelector(".nav__Bar")
+let naver__Bar =document.querySelectorAll(".naver__Bar")
 
+ humburgerIcon.addEventListener("click",function (){ 
+     humburgerIcon.classList.toggle("humburger--active")
+    header.classList.toggle("header--active")
+    nav__bar.classList.toggle("Bar--active")
+ }
+ )
+ naver__Bar.forEach(element => {
+    element.addEventListener("click",function (){ 
+     humburgerIcon.classList.toggle("humburger--active")
+    header.classList.toggle("header--active")
+    nav__bar.classList.toggle("Bar--active")
+ }
+ )
+})
